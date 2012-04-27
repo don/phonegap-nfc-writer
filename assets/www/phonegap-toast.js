e5ba47189ae2d76607678c640a63e6eec24f495c
Don Coleman <dcoleman@chariotsolutions.com>
2012-04-27 02:35:05 -0400

    updated (forked) version of Toasty

--- assets/www/phonegap-toast.js
+++ assets/www/phonegap-toast.js
@@ -1,17 +1,17 @@
-/*global PhoneGap*/
+/*global cordova*/
 var Toasty = function() {
 };
 
 Toasty.prototype.showLong = function(message, win, fail) {
-  PhoneGap.exec(win, fail, "Toasty", "show_long", [message]);
+  cordova.exec(win, fail, "Toasty", "show_long", [message]);
 };
 
 Toasty.prototype.showShort = function(message, win, fail) {
-  PhoneGap.exec(win, fail, "Toasty", "show_short", [message]);
+  cordova.exec(win, fail, "Toasty", "show_short", [message]);
 };
 
 Toasty.prototype.cancel = function(win, fail) {
-  PhoneGap.exec(win, fail, "Toasty", "cancel", []);
+  cordova.exec(win, fail, "Toasty", "cancel", []);
 };
 
 navigator.toast = new Toasty();
